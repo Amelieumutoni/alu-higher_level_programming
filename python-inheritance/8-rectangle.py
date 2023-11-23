d8072d1d9d87bca13b3c70c8a7fc44bb918cc676
Amelieumutoni <a.umutoni@alustudent.com>
2023-11-23 13:12:04 -0800

    my commits

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,12 @@
-#!/usr/bin/python3
-"""Module to design a rectangle"""
-
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
-
+from 7-base_geometry import BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle"""
     def __init__(self, width, height):
-        """Initializes width and height of Rectangle
-            Args:
-                width(int): value of width of rectangle
-                height(int): value of height of rectangle
-        """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        BaseGeometry.integer_validator("width", width)
+        BaseGeometry.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
