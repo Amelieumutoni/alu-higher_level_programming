f11a2e55c89b2c73493e8970309e49feb2565d4f
Amelieumutoni <a.umutoni@alustudent.com>
2023-11-23 13:17:43 -0800

    my commits

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
-from 7-base_geometry import BaseGeometry
 
-class Rectangle(BaseGeometry):
+class Rectangle:
+    __nb_instances = 0
+
     def __init__(self, width, height):
-        BaseGeometry.integer_validator("width", width)
-        BaseGeometry.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        self._width = width
+        self._height = height
+        self.integer_validator(self._width)
+        self.integer_validator(self._height)
+        self.id = Rectangle.__nb_instances
+        Rectangle.__nb_instances += 1
 
     def area(self):
-        return self.__width * self.__height
+        return self._width * self._height
 
+    @staticmethod
+    def integer_validator(value):
+        if not isinstance(value, int) or value <= 0:
+            raise TypeError('Value must be a positive integer')
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r._width, r._height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
