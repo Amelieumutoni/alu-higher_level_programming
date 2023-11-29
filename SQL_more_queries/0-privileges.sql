c0ed2e0f0e18654d3c596a3f4fc52785b00d7f23
Amelieumutoni <a.umutoni@alustudent.com>
2023-11-29 09:26:52 -0800

    my commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- A  script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SELECT USER, HOST, GRANTEE, GRANTEETYPE, TABLE_CATALOG, PRIVILEGE_TYPE, IS_GRANTABLE 
+FROM INFORMATION_SCHEMA.USER_PRIVILEGES 
+WHERE USER IN ('user_0d_1', 'user_0d_2') AND HOST = 'localhost';
