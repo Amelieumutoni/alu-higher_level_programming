d3d7208762c2555c1f870d5d487331c55a8333fb
Amelieumutoni <a.umutoni@alustudent.com>
2023-11-30 23:30:03 -0800

    network

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A script POSt Header variable to the URL email, subject
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# Curl sends POST req to URL, display response body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
