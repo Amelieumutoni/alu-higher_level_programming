5dd2c674c0e64873e6cd783b8fb4720a0be38a74
Amelieumutoni <a.umutoni@alustudent.com>
2024-02-03 06:51:43 -0800

    3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
+module.exports = class Rectangle {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
-      (this.width = w), (this.height = h);
+      this.width = w;
+      this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-      }
-      console.log('');
+      console.log('X'.repeat(this.width));
     }
   }
-}
-module.exports = Rectangle;
+};
